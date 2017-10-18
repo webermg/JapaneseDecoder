40df7cb22f997f6ca2eb3b122f2f792fcc8de1cd
webermg <matthew.gerald.weber@gmail.com>
2017-10-18 09:07:19 -0700

    Update Decoder.cs

--- Decoder.cs
+++ Decoder.cs
@@ -8,9 +8,33 @@ namespace JapanesePracticer
 {
     class Decoder
     {
+        //external file names
+        public static const HIRAGANA_FILE = "hiragana.txt";
+        public static const KATAKANA_FILE = "katakana.txt";
+        
+        //external files containing kana to romaji relations
+        private static File hiraganaFile;
+        private static File katakanaFile;
+        
+        //arrays containing kana for use in decoding
+        private Hashtable hiragana;
+        private Hashtable katakana;
+        
         public Decoder() {
+            //initialize files
+            loadKanaArrays();
         }
         
+        /**
+            takes in a string of kana and returns a string of romaji
+        */
+        public string getRomaji(string input) {
+        }
+        
+        private void loadKanaArrays() {
+            //initialize files
+            //load kana arrays from files
+        }
         
     }
 }
